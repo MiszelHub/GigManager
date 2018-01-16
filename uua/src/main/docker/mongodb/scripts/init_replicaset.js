24e98f3746782a9f40b3e9e824ead5b38e9182f7
andrzejczak195556 <nolanb.o.s@gmail.com>
2018-01-16 22:52:32 +0100

    changed mongo ports

--- uua/src/main/docker/mongodb/scripts/init_replicaset.js
+++ uua/src/main/docker/mongodb/scripts/init_replicaset.js
@@ -4,8 +4,8 @@ if(status.errmsg === 'no replset config has been received') {
 }
 for (var i = 1; i <= param; i++) {
     if(i!==1)
-        rs.add(folder+"_uua-mongodb-node_" + i + ":27017");
+        rs.add(folder+"_uua-mongodb-node_" + i + ":27018");
 }
 cfg = rs.conf();
-cfg.members[0].host = folder+"_uua-mongodb-node_1:27017";
+cfg.members[0].host = folder+"_uua-mongodb-node_1:27018";
 rs.reconfig(cfg);
